0fd2addb16a2c3b836a0fe9125e7e7fa323f1998
Chad Parnin <chadparnin@gmail.com>
2015-12-13 18:06:56 -0500

    Changed Share.java
    Can send an email, but cannot attach db

--- app/src/main/java/chad/budgetkeeper/Share.java
+++ app/src/main/java/chad/budgetkeeper/Share.java
@@ -5,12 +5,14 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import java.io.File;
 
-public class Share extends Activity implements View.OnClickListener
+public class Share extends Activity
 {
 
     private Button sendBtn;
@@ -22,32 +24,54 @@ public class Share extends Activity implements View.OnClickListener
         setContentView(R.layout.share);
 
         sendBtn = (Button) findViewById(R.id.send);
-        sendBtn.setOnClickListener(this);
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                sendEmail();
+            }
+        });
     }
 
-    /*  This is set up (I believe) to open the email app
-        then attach the "database" in the email...I think
-    */
-    public void onClick(View view)
+    public void sendEmail()
     {
-        if (view.getId() == R.id.send)
-        {
-            Intent emailIntent = new Intent(Intent.ACTION_SEND);
-            emailIntent.setType("text/plain");
-            emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[] {"email@example.com"});
-            emailIntent.putExtra(Intent.EXTRA_SUBJECT, "subject here");
-            emailIntent.putExtra(Intent.EXTRA_TEXT, "body text");
-            File root = Environment.getExternalStorageDirectory();
-            // following database path may need changed
-            String pathToMyAttachedFile = "/data/data/chad.budgetkeeper/databases";
-            File file = new File(root, pathToMyAttachedFile);
-            if (!file.exists() || !file.canRead()) {
-                return;
-            }
-            Uri uri = Uri.fromFile(file);
-            emailIntent.putExtra(Intent.EXTRA_STREAM, uri);
-            startActivity(Intent.createChooser(emailIntent, "Pick an Email provider"));
+        Log.i("Send email", "");
+        String[] TO = {""};
+        String[] CC = {""};
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+
+        emailIntent.setData(Uri.parse("mailto:"));
+        emailIntent.setType("text/plain");
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
+        emailIntent.putExtra(Intent.EXTRA_CC, CC);
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Your subject");
+        emailIntent.putExtra(Intent.EXTRA_TEXT, "Email message goes here");
+
+        try {
+            startActivity(Intent.createChooser(emailIntent, "Send mail..."));
+            finish();
+            Log.i("Finished...", "");
         }
+        catch (android.content.ActivityNotFoundException ex)
+        {
+            Toast.makeText(Share.this, "There is no email client installed.", Toast.LENGTH_SHORT).show();
+        }
+
+        File root = Environment.getExternalStorageDirectory();
+        // following database path may need changed
+        String pathToMyAttachedFile = "/data/data/chad.budgetkeeper/databases";
+        File file = new File(root, pathToMyAttachedFile);
+        if (!file.exists() || !file.canRead()) {
+            return;
+        }
+        Uri uri = Uri.fromFile(file);
+        emailIntent.putExtra(Intent.EXTRA_STREAM, uri);
+        startActivity(Intent.createChooser(emailIntent, "Pick an Email provider"));
     }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.share, menu);
+//        return true;
+//    }
 
 }
